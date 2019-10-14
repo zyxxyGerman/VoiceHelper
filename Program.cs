ffeb116f4b8c6792033cb39d1e2b4244bfaac348
German <wowblitz45@gmail.com>
2019-10-14 19:42:10 +0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using helperform.Commands;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace helperform
+{
+    public static class Program
+    {
+        public static List<ICommand> Commands = new List<ICommand>
+            {
+             new OpenGoogleCommand()
+            ,new OpenFacebookCommand()
+            ,new OpenYouTubeCommand()
+            ,new OpenYandexCommand()
+            ,new OpenMs45Command()
+            ,new OpenCoursesCommand()
+            ,new OpenSearchCommand()
+            ,new CloseProgramm()
+            };
+
+        [STAThread]
+        static void Main(String[] agrs)
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new MainMenu());
+        }
+    }
+}
